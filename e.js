04e61214bc5cc0fc306c7d92b09ea4cc22b1ec15
blazecoderman <50154384+blazecoderman@users.noreply.github.com>
2020-01-12 15:33:01 -0800

    Update e.js

--- e.js
+++ e.js
@@ -1,5 +1,11 @@
 var num=0;
-var count=function(){
+var updatecount=function(){
 num=num+1;
 console.log("num=",num);
 }
+
+function func1() {
+document.getElementById("count").addEventListener("click", updatecount);}
+window.onload=func1;
+
+
